834271d051e8a530322b74da45c3a3ab70585954
Masaki Takeyama <takenoko000jp@gmail.com>
2020-11-24 18:04:10 +0900

    fix scroll to top

--- js/main.js
+++ js/main.js
@@ -10,3 +10,35 @@ $(function(){
 		});
 	});
 });
+var pageTop = document.getElementById("page-top");
+var yws_pagetop = function() {
+    if (window.pageYOffset > 400) {
+		pageTop.classList.add("fixed")
+    } else {
+		pageTop.classList.remove("fixed")
+    }
+};
+
+window.addEventListener("load", yws_pagetop);
+window.addEventListener("scroll", yws_pagetop);
+
+scrollTop('page-top', 500);
+
+function scrollTop(elem, duration) {
+	let target = document.getElementById(elem);
+	target.addEventListener('click', function() {
+		let currentY = window.pageYOffset; 
+		let step = duration/currentY > 1 ? 10 : 100;
+		let timeStep = duration/currentY * step;
+		let intervalID = setInterval(scrollUp, timeStep);
+	
+		function scrollUp(){
+			currentY = window.pageYOffset;
+			if(currentY === 0) {
+				clearInterval(intervalID);
+			} else {
+				scrollBy( 0, -step );
+			}
+		}
+	});
+}
